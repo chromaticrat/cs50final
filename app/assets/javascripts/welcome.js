29c8210fa04f211d1e27303413763c15f924cabf
chromaticrat <kctullis@gmail.com>
2014-11-16 19:28:03 -0800

    speedometer implemented

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,13 +2,14 @@
 var timer1; // total game time elapsed
 var timer2; // time since last keydown
 var timer3; // time since started typing current word
-var previous; // timestamp of most recent keypress
+var previousTime; // timestamp of most recent keypress
 var wordspeed = 0; // time taken to type current word
+var previousKey; // last key typed
+var newKey; // current key pressed
 var wordlength = 1; // length of current word
 var wordclass; // css class for completed word
 var keycount = 0; // total characters displayed in div#game
 var wpm = 0;
-var typed;
 var speeds = [];
 
 // Helper function to start game
@@ -22,102 +23,119 @@ function start(event) {
 
 // TODO: Helper function to handle backspace
 function back(key) {
-
-
+	var elements = document.getElementsByClassName("current");
+	var current = elements[0].innerHTML;
+	elements[0].innerHTML = current.substr(0, current.length - 1);
 	// shorten count of current word length and total length
 	wordlength --;
 	keycount --;
+	// TEST
+	//$("div#test").append("Called 'back()'. <br />Wordlength = "
+	//+wordlength+" and Keycount = "+keycount+"<br />");
+}
+
+// Helper function to update speedometer
+// changes CSS width of the div with id of "speedbar-cover", according to wpm)
+function speedbar(){
+	var coverage = (Math.abs(wpm/2 - 100)).toString();
+	var coverWidth = coverage.concat("%");
+	$("div#speedbar-cover").css("width", coverWidth);
 }
 
 // Helper function to calculate and display speed
 function processSpeed(event) {
 	var now = event.timeStamp;
 	// Calculate speeds
-	timer2 = now - previous; // get this character speed
-	$("div#speed").append(timer2+",  "); // TEST timer2
+	timer2 = now - previousTime; // get this character speed
+	//$("div#speed").append(timer2+",  "); // TEST timer2
 	
 	// get words per min assuming every 5 characters is a word
 	wpm = Math.floor((keycount/5) / ((now - timer1)/60000));
 	
-	// space (32) or return (13) define word break for formatting 
+	// space (32) or return (13) define word break for formatting
+	// TODO: ignore keypress if previousTime key was also one of these 
 	if (event.which == 32 || event.which == 13) {
 		wordspeed = Math.floor(60000/(now - timer3)); //wpm rate for just this word
 		timer3 = now;
 		wordlength = 0;
 	}
+	speedbar();
 }
 
-// choose color for each word as it is completed
+// choose color according to typing rate
 function getcolor(){
-	if (wordspeed < 35) {
+	switch (true) {
+	case wordspeed < 35:
 		wordclass = "vslow";
-	}
-	else if (wordspeed < 45) {
+		break;
+	case wordspeed < 45:
 		wordclass = "slow";
-	}
-	else if (wordspeed < 55) {
+		break;
+	case wordspeed < 55:
 		wordclass = "medium_slow";
-	}
-	else if (wordspeed < 65) {
+		break;
+	case wordspeed < 65:
 		wordclass = "medium";
-	}
-	else if (wordspeed < 75) {
+		break;
+	case wordspeed < 75:
 		wordclass = "medium_fast";
-	}
-	else if (wordspeed < 85) {
+		break;
+	case wordspeed < 85:
 		wordclass = "fast";
-	}
-	else {
+		break;
+	default:
 		wordclass = "vfast";
 	}
-	
 }
 
-
-// Helper function to style and display typed input
+// Helper function to style and display newKey input
 function display() { 
 	// if this is the first character of the game
 	if (timer2 == 0) {
-		$("div#game").html("<span class='current'>"+typed+"</span>");
+		$("div#game").append("<span class='current'>"+newKey+"</span>");
 	}
 	// display completed word
 	else if (wordlength == 0) {
 		// Get color class for word based on speed
 		getcolor();
 		// apply color 
-		$("span.current").append(typed)
+		$("span.current").append(newKey)
 			.removeClass("current")
 			.addClass(wordclass);
-		$("div#game").append("<span class='current'>&nbsp;</span>");
+		$("div#game").append("<span class='current'></span>");
 	}
 	// display new character of word in progress
 	else {
-		$("span.current").append(typed);
+		$("span.current").append(newKey);
 	}
 	// display speed
 	$("div#score").html("Average Speed: "+wpm+" wpm");
 	$("div#score").append("<br />Speed Of Most Recent Word: "+wordspeed+" wpm");
 }
 
-// Main Function
+// Main Function: Handler for Keypress events in the window
 $(window).keypress(function (event) {    
-	// prevent default events 
+	// prevent default events so insertion point can't be changed by user
 	event.preventDefault();
     // make a string from whatever key the user typed
-    typed = String.fromCharCode(event.which);
+    newKey = String.fromCharCode(event.which);
+    // TEST input
+    //$("div#test").append("user typed "+event.which+" which is '"+newKey+"', ");
     // if game already underway, keep playing
-    if (previous) {
+    if (previousTime) {
         // if the key pressed is Backspace
-    	if (event.which == 8) { back(); }
+    	//if (event.which == 8) { back(); }
     	// if not Backspace
-		else { processSpeed(event); }
+		//else { 
+		processSpeed(event); 
+		//}
     }
     // if this is the first character, start the game
     else { start(event); }
     // display keyboard input
 	display();
     // keep track of when most recent keyboard input received
-    previous = event.timeStamp; 
+    previousTime = event.timeStamp; 
     wordlength ++;
 	keycount ++;
 });
@@ -137,8 +155,8 @@ $(window).keypress(function (event) {
 //        wordspeed += timer2;
 // 	?TODO: display game time elapsed
 //TODO: format and display user input -- color depends on speed    
-//		if typed is not a space or return, print character in black 
-//		else if typed is a space or return, 
+//		if newKey is not a space or return, print character in black 
+//		else if newKey is a space or return, 
 //			change color of completed word, print space or return
 //  		and start tracking next word
 
@@ -164,26 +182,26 @@ $(window).keypress(function (event) {
 // 	"which is "+wpm+" words per minute.");
 
 
-// style each character typed based on time elapsed since previous
+// style each character typed based on time elapsed since previous time
 //     if (timer2 < 50) {
-//         $("div#game").append("<span class='vfast'>"+typed+"</span>");
+//         $("div#game").append("<span class='vfast'>"+newKey+"</span>");
 //     } 
 //     else if (timer2 < 60) {
-//         $("div#game").append("<span class='fast'>"+typed+"</span>");
+//         $("div#game").append("<span class='fast'>"+newKey+"</span>");
 //     } 
 //     else if (timer2 < 80){
-//         $("div#game").append("<span class='medium_fast'>"+typed+"</span>");
+//         $("div#game").append("<span class='medium_fast'>"+newKey+"</span>");
 //     }
 //     else if (timer2 < 110) {
-//         $("div#game").append("<span class='medium'>"+typed+"</span>");
+//         $("div#game").append("<span class='medium'>"+newKey+"</span>");
 //     }
 //     else if (timer2 < 140) {
-//         $("div#game").append("<span class='medium_slow'>"+typed+"</span>");
+//         $("div#game").append("<span class='medium_slow'>"+newKey+"</span>");
 //     }
 //     else if (timer2 < 160) {
-//         $("div#game").append("<span class='slow'>"+typed+"</span>");
+//         $("div#game").append("<span class='slow'>"+newKey+"</span>");
 //     }
 //     else {
-//         $("div#game").append("<span class='vslow'>"+typed+"</span>");
+//         $("div#game").append("<span class='vslow'>"+newKey+"</span>");
 //     }
 
